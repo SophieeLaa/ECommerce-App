a6322cfcb5f42a595b0c26769b69c5f59e27b50b
sophie 888 <sophie88998899@gmail.com>
2023-11-29 12:26:18 +0400

    Adding views and modifing controllers

--- eCom/Controllers/ActorsController.cs
+++ eCom/Controllers/ActorsController.cs
@@ -13,7 +13,7 @@ namespace eCom.Controllers
         public IActionResult Index()
         {
             var data = _context.Actors.ToList();
-            return View();
+            return View(data);
         }
     }
 }
