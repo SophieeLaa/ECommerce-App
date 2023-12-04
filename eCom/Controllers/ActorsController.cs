7862e2705d1061817e1cadf2cdb99c83b106bf15
sophie 888 <sophie88998899@gmail.com>
2023-12-04 18:14:15 +0400

    Some modification

--- eCom/Controllers/ActorsController.cs
+++ eCom/Controllers/ActorsController.cs
@@ -15,18 +15,18 @@ namespace eCom.Controllers
         }
         public async Task<IActionResult> Index()
         {
-            var data = await  _service.GetAllAsync();
+            var data = await _service.GetAllAsync();
             return View(data);
         }
         //Get Request
 
 
         public IActionResult Create()
-        { 
+        {
             return View();
         }
         [HttpPost]
-        public async Task<IActionResult> Create( [Bind("FullName, ProfilePictureURL,Bio")]Actor actor)
+        public async Task<IActionResult> Create([Bind("FullName, ProfilePictureURL,Bio")] Actor actor)
         {
             if (!ModelState.IsValid)
             {
@@ -36,11 +36,28 @@ namespace eCom.Controllers
             return RedirectToAction(nameof(Index));
         }
 
-        public async Task<IActionResult> Details (int id)
+        public async Task<IActionResult> Details(int id)
         {
             var actorDetails = await _service.GetByIdAsync(id);
-            if (actorDetails == null) return View("Empty");
-            return View(actorDetails); 
+            if (actorDetails == null) return View("NotFound");
+            return View(actorDetails);
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            var actorDetails = await _service.GetByIdAsync(id);
+            if (actorDetails == null) return View("NotFound");
+            return View(actorDetails);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,FullName,ProfilePictureURL,Bio")] Actor actor)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(actor);
+            }
+            await _service.UpdateAsync(id,actor);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
